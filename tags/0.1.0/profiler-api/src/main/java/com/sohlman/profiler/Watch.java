89e8f89efad5226d6fd73a04fcaddbbb9ea73253
sampsa.sohlman <sampsa.sohlman@0054a7b8-c2f6-3505-723a-148ad038c3e7>
2010-10-05 05:03:07 +0000

    0.1.0 version

---
+++ tags/0.1.0/profiler-api/src/main/java/com/sohlman/profiler/Watch.java
@@ -0,0 +1,162 @@
+/*
+   Copyright 2010 Sampsa Sohlman http://sampsa.sohlman.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.sohlman.profiler;
+
+public class Watch {
+	private static String PROFILER_NOT_STOPPED_CORRECTLY = "PROFILER NOT STOPPED CORRECTLY";
+	private String methodName;
+	private String className;
+	private String text;
+	private long start = 0;
+	private long end = 0;
+	private long duration = -1;
+	private int level;
+
+	Watch(Watch parent) {
+		if (parent == null) {
+			this.level = 0;
+		} else {
+			this.level = parent.getLevel() + 1;
+		}
+		this.parent = parent;
+		start = System.currentTimeMillis();
+	}
+
+	private Watch next = null;
+	private Watch firstChild = null;
+	private Watch lastChild = null;
+	private Watch parent = null;
+
+	void setNext(Watch watch) {
+		next = watch;
+	}
+
+	public int getLevel() {
+		return this.level;
+	}
+
+	void addChild(Watch watch) {
+		if (firstChild == null) {
+			firstChild = watch;
+			lastChild = watch;
+		} else {
+			lastChild.setNext(watch);
+			lastChild = watch;
+		}
+		watch.parent = this;
+	}
+
+	Watch getNext() {
+		return next;
+	}
+
+	Watch getParent() {
+		return parent;
+	}
+
+	Watch getFirstChild() {
+		return firstChild;
+	}
+	
+	Watch getLastChild() {
+		return lastChild;
+	}
+
+	public long getTimeToNextMillis() {
+		if (getFirstChild() != null) {
+			return getFirstChild().getStartTimeMillis() - getStartTimeMillis();
+		}
+		else if (getNext() != null) {
+			return getNext().getStartTimeMillis() - getEndTimeMillis();
+		} 
+		else {
+			return getMillisFromParent(getParent(), this);
+		}
+	}
+
+	private static long getMillisFromParent(Watch parent, Watch current) {
+		// This is recursive
+		if(parent==null ) {
+			return 0;
+		} if (parent.getNext()!=null) {
+			return parent.getNext().getStartTimeMillis()
+					- current.getStartTimeMillis();
+		} else if (parent.getParent() != null) {
+			return getMillisFromParent(parent.getParent(), current);
+		} else {
+			return parent.getEndTimeMillis() - current.getEndTimeMillis();
+		}
+	}
+
+	public boolean isRoot() {
+		return parent == null;
+	}
+
+	public boolean hasChilds() {
+		return firstChild != null;
+	}
+
+	public boolean hasNext() {
+		return next != null;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	public String getClassName() {
+		return this.className;
+	}
+
+	public String getMethodName() {
+		return this.methodName;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		return this == object;
+	}
+
+	void stop(String className, String methodName, String text) {
+		if (this.end == 0) {
+			this.end = System.currentTimeMillis();
+			this.duration = this.end - this.start;
+			
+			this.className = className;
+			this.methodName = methodName;
+			this.text = text;
+			if( lastChild!=null) {
+				lastChild.stop(null, null,PROFILER_NOT_STOPPED_CORRECTLY);
+			}
+		}
+	}
+
+	public long getElapsedInMillis() {
+		return this.duration;
+	}
+
+	public long getStartTimeMillis() {
+		return this.start;
+	}
+
+	public long getEndTimeMillis() {
+		return this.end;
+	}
+
+	public boolean isRunning() {
+		return this.duration == -1;
+	}
+}
---
+++ tags/0.1.0/profiler-api/src/test/java/com/sohlman/profiler/ThreadProfilerTest.java
@@ -0,0 +1,341 @@
+/*
+   Copyright 2010 Sampsa Sohlman http://sampsa.sohlman.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.sohlman.profiler;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ThreadProfilerTest {
+	
+	
+	
+	private void method(int level, int maxLevel, int loopCount) {
+		method(level, maxLevel, loopCount, -1, 0);
+	}
+
+	private void method(int level, int maxLevel, int loopCount,
+			int noStopLevel, int loopCounter) {
+		sleep(10);
+		Watch watch = ThreadLocalProfiler.start();
+		sleep(10);
+		try {
+			if (level < maxLevel) {
+				for (int i = 0; i < loopCount; i++) {
+					sleep(10);
+					method(level + 1, maxLevel, loopCount, noStopLevel, i);
+				}
+			}
+		} finally {
+			if (level != noStopLevel) {
+				ThreadLocalProfiler.stop(watch, "com.sohlman.profiler.test."
+						+ level + ".Doit", "method" + loopCounter);
+			}
+		}
+	}
+	
+	public void sleep(long millis) {
+		try {
+			Thread.currentThread().sleep(millis);
+		} catch (InterruptedException e) {
+			// Ignore
+		}
+	}
+	
+	@Test
+	public void testDefaults() {
+		// This test should be first. If the default values are
+		// changed then modify also teardown()
+		Assert.assertFalse(ThreadLocalProfiler.isSetupRequired());
+		Assert.assertFalse(ThreadLocalProfiler.isDisabled());
+	}
+	
+	@Test
+	public void testSimple() throws Exception {
+		Watch watch = ThreadLocalProfiler.start();
+		Assert.assertTrue(watch.isRunning());
+		ThreadLocalProfiler.stop(watch, "/root");
+		Assert.assertFalse(watch.isRunning());
+		Watch[] watches = ThreadLocalProfiler.report();
+		Assert.assertEquals(1, watches.length);
+		ThreadLocalProfiler.tearDown();
+	}
+	
+	private String THRESHOLD = "THRESHOLD";
+	private String ROWINDENTIFIER = "ROWINDENTIFIER";
+	
+	@Test
+	public void testRecursiveWithThreshold() throws Exception {
+		method(0, 2, 5);
+		Watch[] watches = ThreadLocalProfiler.report();
+		
+		String reportString = ToStringUtil.writeReport(watches, 10, THRESHOLD,ROWINDENTIFIER,"\n");
+		
+		System.out.println(reportString);
+		
+		verifyTree(watches);
+		Assert.assertEquals(31, watches.length);
+		Assert.assertTrue("Verify report threshold from text report", reportString.indexOf(THRESHOLD)>=0);
+		Assert.assertTrue("Verify report row identifier from text report",reportString.indexOf(ROWINDENTIFIER)>=0);
+		
+		ThreadLocalProfiler.tearDown();
+	}
+	
+	@Test
+	public void testRecursiveWithWithoutThreshold() throws Exception {
+		method(0, 1, 5);
+		Watch[] watches = ThreadLocalProfiler.report();
+		
+		// use big threshold
+		String reportString = ToStringUtil.writeReport(watches, 10000, THRESHOLD,ROWINDENTIFIER,"\n");
+		
+		System.out.println(reportString);
+		
+		verifyTree(watches);
+		Assert.assertEquals(6, watches.length);
+		
+		Assert.assertFalse("Verify report threshold from text report", reportString.indexOf(THRESHOLD)>=0);
+		Assert.assertTrue("Verify report row identifier from text report",reportString.indexOf(ROWINDENTIFIER)>=0);
+		
+		ThreadLocalProfiler.tearDown();
+	}	
+
+	private void verifyTree(Watch[] watches) throws Exception {
+		long lastStart = watches[0].getStartTimeMillis();
+		long lastElapsed = watches[0].getElapsedInMillis();
+		long lastToNextMillis = watches[0].getTimeToNextMillis();
+		long totalMillis = watches[0].getElapsedInMillis();
+		long millisCounter=watches[0].getTimeToNextMillis();
+		
+		for(int i=1 ; i< watches.length ; i++) {
+			millisCounter =+ watches[i].getElapsedInMillis();
+			millisCounter =+ watches[i].getElapsedInMillis();			
+			
+		}
+		
+		Assert.assertEquals(totalMillis, totalMillis);
+	}
+	
+	@Test
+	public void testNotAllStopped() throws Exception {
+		Watch w1 = ThreadLocalProfiler.start();
+		sleep(1);
+		Watch w2 = ThreadLocalProfiler.start();
+		sleep(1);
+		Watch w3 = ThreadLocalProfiler.start();
+		sleep(1);
+		ThreadLocalProfiler.stop(w3, "w3");		
+		ThreadLocalProfiler.stop(w1, "w1");
+		
+		Assert.assertEquals(w1.getEndTimeMillis(), w2.getEndTimeMillis());
+		Assert.assertNotSame(w1.getEndTimeMillis(), w3.getEndTimeMillis());
+		Assert.assertNotNull(ThreadLocalProfiler.report());
+	}	
+	
+	@Test
+	public void testVariations1() throws Exception {
+		Watch w1 = ThreadLocalProfiler.start();
+		sleep(1);
+		Watch w2 = ThreadLocalProfiler.start();
+		sleep(1);
+		Watch w3 = ThreadLocalProfiler.start();
+		sleep(1);
+		ThreadLocalProfiler.stop(w3, "w3");	
+		ThreadLocalProfiler.stop(w2, "w2");
+		Watch w4 = ThreadLocalProfiler.start();
+		ThreadLocalProfiler.stop(w4, "w4");
+		ThreadLocalProfiler.stop(w1, "w1");
+		
+		Assert.assertEquals(w1.getEndTimeMillis(), w2.getEndTimeMillis());
+		Assert.assertNotSame(w1.getEndTimeMillis(), w3.getEndTimeMillis());
+		Assert.assertEquals(4, ThreadLocalProfiler.report().length);
+	}
+	
+	@Test
+	public void testVariations2() throws Exception {
+		Watch w1 = ThreadLocalProfiler.start();
+		sleep(1);
+		Watch w2 = ThreadLocalProfiler.start();
+		sleep(1);
+		Watch w3 = ThreadLocalProfiler.start();
+		sleep(1);
+		ThreadLocalProfiler.stop(w3, "w3");	
+		ThreadLocalProfiler.stop(w2, "w2");
+		Watch w4 = ThreadLocalProfiler.start();
+		ThreadLocalProfiler.stop(w4, "w4");
+		Watch w5 = ThreadLocalProfiler.start();
+		Watch w6 = ThreadLocalProfiler.start();
+		ThreadLocalProfiler.stop(w4, "w6");			
+		ThreadLocalProfiler.stop(w4, "w5");		
+		ThreadLocalProfiler.stop(w1, "w1");
+		
+		Assert.assertEquals(w1.getEndTimeMillis(), w2.getEndTimeMillis());
+		Assert.assertNotSame(w1.getEndTimeMillis(), w3.getEndTimeMillis());
+		Assert.assertEquals(6, ThreadLocalProfiler.report().length);
+	}	
+	
+	@Test
+	public void testThreadPoolWithoutTearDown() throws Exception {
+		Watch w1 = ThreadLocalProfiler.start();
+		sleep(1);
+		Watch w2 = ThreadLocalProfiler.start();
+		sleep(1);
+		ThreadLocalProfiler.stop(w2, "w2");		
+		ThreadLocalProfiler.stop(w1, "w1");
+		
+		Watch w3 = ThreadLocalProfiler.start();
+		sleep(1);
+		Watch w4 = ThreadLocalProfiler.start();
+		sleep(1);
+		ThreadLocalProfiler.stop(w3, "w3");		
+		ThreadLocalProfiler.stop(w4, "w4");
+		
+		Assert.assertEquals(2,ThreadLocalProfiler.report().length);		
+	}
+	
+	@Test 
+	public void testDisabled() {
+		ThreadLocalProfiler.setDisabled(true);
+		Assert.assertNull(ThreadLocalProfiler.start());
+		ThreadLocalProfiler.setDisabled(false);
+		Assert.assertNotNull(ThreadLocalProfiler.start());
+	}
+	
+	@Test 
+	public void testSetupRequiredTrue_Start_Setup_Start() {
+		ThreadLocalProfiler.setSetupRequired(true);
+		Assert.assertNull(ThreadLocalProfiler.start());
+		ThreadLocalProfiler.setUp();
+		Assert.assertNotNull(ThreadLocalProfiler.start());
+	}	
+	
+	@Test 
+	public void testSetupFalse_Start_SetupTrue_Stop() {
+		ThreadLocalProfiler.setSetupRequired(false);
+		Watch watch = ThreadLocalProfiler.start();
+		ThreadLocalProfiler.setSetupRequired(true);
+		ThreadLocalProfiler.stop(watch,"");
+		Assert.assertEquals(1, ThreadLocalProfiler.report().length);
+		Assert.assertNull(ThreadLocalProfiler.start());
+	}		
+	
+	@Test 
+	public void testSetupTrue_Start_SetupFalse_Stop_Start_Stop() {
+		ThreadLocalProfiler.setSetupRequired(true);
+		ThreadLocalProfiler.setUp();
+		Watch watch = ThreadLocalProfiler.start();
+		Assert.assertNotNull(watch);
+		ThreadLocalProfiler.setSetupRequired(false);
+		ThreadLocalProfiler.stop(watch,"");
+		Assert.assertEquals(1, ThreadLocalProfiler.report().length);
+		
+		watch = ThreadLocalProfiler.start();
+		Assert.assertNotNull(watch);
+		ThreadLocalProfiler.setSetupRequired(false);
+		ThreadLocalProfiler.stop(watch,"");
+		Assert.assertEquals(1, ThreadLocalProfiler.report().length);
+	}
+	
+	@Test 
+	public void testSetupTrue_Start_SetupFalse_Stop_Setup_Start_Stop() {
+		ThreadLocalProfiler.setSetupRequired(true);
+		ThreadLocalProfiler.setUp();
+		Watch watch = ThreadLocalProfiler.start();
+		Assert.assertNotNull(watch);
+		ThreadLocalProfiler.setSetupRequired(false);
+		ThreadLocalProfiler.stop(watch,"");
+		Assert.assertEquals(1, ThreadLocalProfiler.report().length);
+		ThreadLocalProfiler.setUp();
+		watch = ThreadLocalProfiler.start();
+		Assert.assertNotNull(watch);
+		ThreadLocalProfiler.setSetupRequired(false);
+		ThreadLocalProfiler.stop(watch,"");
+		Assert.assertEquals(1, ThreadLocalProfiler.report().length);
+	}	
+	
+	@Test 
+	public void testSetupTrue_Start_Disabled_Stop_Setup_Start() {
+		ThreadLocalProfiler.setSetupRequired(true);
+		ThreadLocalProfiler.setUp();
+		Watch watch = ThreadLocalProfiler.start();
+		Assert.assertNotNull(watch);
+		ThreadLocalProfiler.setDisabled(true);
+		ThreadLocalProfiler.stop(watch,"");
+		Assert.assertEquals(1, ThreadLocalProfiler.report().length);
+		
+		watch = ThreadLocalProfiler.start();
+		Assert.assertNull(watch);
+	}	
+	
+	@Test 
+	public void testTestDisabledDuringRunning() {
+		ThreadLocalProfiler.setSetupRequired(false);
+		
+		Watch w1 = ThreadLocalProfiler.start();
+		Assert.assertNotNull(w1);
+		Watch w2 = ThreadLocalProfiler.start();
+		Assert.assertNotNull(w2);
+		Watch w3 = ThreadLocalProfiler.start();
+		Assert.assertNotNull(w3);		
+		ThreadLocalProfiler.stop(w3,"");
+		ThreadLocalProfiler.setDisabled(true);
+		Watch w4 = ThreadLocalProfiler.start();
+		Assert.assertNotNull(w4);		
+		ThreadLocalProfiler.stop(w4,"");		
+		ThreadLocalProfiler.stop(w2,"");
+		ThreadLocalProfiler.stop(w1,"");
+		Assert.assertEquals(4, ThreadLocalProfiler.report().length);
+
+		//
+		Assert.assertNull(ThreadLocalProfiler.start());
+		ThreadLocalProfiler.setDisabled(false);
+		Assert.assertNotNull(ThreadLocalProfiler.start());
+	}		
+	
+	@Test 
+	public void testTestSetUpRequiedDuringRunning() {
+		ThreadLocalProfiler.setSetupRequired(false);
+		
+		Watch w1 = ThreadLocalProfiler.start();
+		Assert.assertNotNull(w1);
+		Watch w2 = ThreadLocalProfiler.start();
+		Assert.assertNotNull(w2);
+		Watch w3 = ThreadLocalProfiler.start();
+		Assert.assertNotNull(w3);		
+		ThreadLocalProfiler.stop(w3,"");
+		ThreadLocalProfiler.setSetupRequired(true);
+		Watch w4 = ThreadLocalProfiler.start();
+		Assert.assertNotNull(w4);		
+		ThreadLocalProfiler.stop(w4,"");		
+		ThreadLocalProfiler.stop(w2,"");
+		ThreadLocalProfiler.stop(w1,"");
+		Assert.assertEquals(4, ThreadLocalProfiler.report().length);
+
+		//
+		Assert.assertNull(ThreadLocalProfiler.start());
+		ThreadLocalProfiler.setUp();
+		Assert.assertNotNull(ThreadLocalProfiler.start());
+	}	
+	
+	@After 
+	public void teardDown() {
+		ThreadLocalProfiler.tearDown();
+		ThreadLocalProfiler.setSetupRequired(false);
+		ThreadLocalProfiler.setDisabled(false);
+	}
+}
