3d2feae48582501ef577c60d53732070d678da75
sampsa.sohlman <sampsa.sohlman@0054a7b8-c2f6-3505-723a-148ad038c3e7>
2010-10-07 05:02:45 +0000

    0.2.0 version tagging

---
+++ tags/0.2.0/profiler-api/src/main/java/com/sohlman/profiler/ToStringUtil.java
@@ -0,0 +1,62 @@
+package com.sohlman.profiler;
+
+public class ToStringUtil {
+	private static void addPrefixes(char c, int length,
+			StringBuilder stringBuilder) {
+		for (int i = 0; i < length; i++) {
+			stringBuilder.append(c);
+		}
+	}
+
+	
+
+	public static String writeReport(Watch[] watches, long alarmThresHoldMillis, String thresholdReached, String rowIdentifier, String lineSeparator) {
+		StringBuilder stringBuilder = new StringBuilder(4096);
+		rowIdentifier = rowIdentifier==null?"":rowIdentifier;
+		long rootMillis=0;
+		long rootDurationMillis=0;
+		if(watches.length>0) {
+			rootMillis=watches[0].getStartTimeMillis();
+			rootDurationMillis=watches[0].getElapsedInMillis();
+		}
+		stringBuilder.append(lineSeparator);
+		stringBuilder.append(String.format("%23s %8s %8s %8s %s ","Timestamp:","Start:", "Elapsed:",
+				"ToNext:","Action:"));
+		
+		if(alarmThresHoldMillis<=rootDurationMillis) {
+			stringBuilder.append(thresholdReached);
+			stringBuilder.append(" ");
+		}		
+		stringBuilder.append(rowIdentifier);
+		stringBuilder.append(lineSeparator);
+
+		for(Watch watch : watches ) {
+			stringBuilder.append(String.format("%1$tY-%1$tm-%1$te %1$tH:%1$tM:%1$tS.%1$tL %2$8d %3$8d %4$8d", watch.getStartTimeMillis(), (watch.getStartTimeMillis() - rootMillis), watch.getElapsedInMillis(), watch.getTimeToNextMillis()));
+			
+			stringBuilder.append(' ');
+			addPrefixes('-', watch.getLevel(), stringBuilder);
+			boolean hasClassInfo=false;
+			if (watch.getClassName() != null && watch.getMethodName() != null) {
+				stringBuilder.append(watch.getClassName()).append('.')
+						.append(watch.getMethodName()).append("(..)");
+				hasClassInfo = true;
+			} 
+			if (watch.getText() != null) {
+				if (hasClassInfo) {
+					stringBuilder.append(" - ");
+				}
+				stringBuilder.append(watch.getText());
+			}			
+			stringBuilder.append(' ');
+			if(alarmThresHoldMillis<=rootDurationMillis) {
+				stringBuilder.append(thresholdReached);
+				stringBuilder.append(" ");
+			}
+			stringBuilder.append(rowIdentifier);
+			stringBuilder.append(lineSeparator);
+		}
+		
+		
+		return stringBuilder.toString();
+	}
+}
---
+++ tags/0.2.0/profiler-spring-support/src/main/java/com/sohlman/profiler/spring/ProfilerInterceptor.java
@@ -0,0 +1,60 @@
+/*
+   Copyright 2010 Sampsa Sohlman http://sampsa.sohlman.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.sohlman.profiler.spring;
+
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.Invocation;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import com.sohlman.profiler.ThreadLocalProfiler;
+import com.sohlman.profiler.Watch;
+
+public class ProfilerInterceptor implements MethodInterceptor {
+	/**
+	 * Create a new PerformanceMonitorInterceptor with a static logger.
+	 */
+	public ProfilerInterceptor() {
+	}
+
+	private Class getClass(Method method, Invocation invocation) {
+		Class declaringClass = method.getDeclaringClass();
+		if (declaringClass.isInstance(invocation.getThis())) {
+			declaringClass = invocation.getThis().getClass();
+		}
+		return declaringClass;
+	}
+
+	/**
+	 * measures time
+	 */
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		Method method = invocation.getMethod();
+		Class invocatedClass = getClass(method, invocation);
+		Watch watch = null;
+		try {
+			watch = ThreadLocalProfiler.start();
+			return invocation.proceed();
+
+		} finally {
+			ThreadLocalProfiler.stop(watch, invocatedClass, method);
+		}
+	}
+
+}
