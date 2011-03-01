8bfce69328e39b7d185db80ce5ad983809b2d760
sampsa.sohlman <sampsa.sohlman@0054a7b8-c2f6-3505-723a-148ad038c3e7>
2011-03-01 06:28:14 +0000

    License dates updates and license information to all files.

--- profiler-api/src/test/java/com/sohlman/profiler/ThreadProfilerTest.java
+++ profiler-api/src/test/java/com/sohlman/profiler/ThreadProfilerTest.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2010 Sampsa Sohlman http://sampsa.sohlman.com
+   Copyright 2010-2011 Sampsa Sohlman http://sampsa.sohlman.com
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
